664fde82250144e075569faa16dd89c424cccbe5
Jim Robinson <jrobinso@broadinstitute.org>
2019-05-02 08:49:20 -0700

    initial commit

---
+++ encode.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python2
+# -*- coding: latin-1 -*-
+'''GET an object from an ENCODE server'''
+
+import requests, json
+
+# Supported file formats
+
+FORMATS = set(["bigBed", "bigWig", "bedpe", "tsv", "hic"])
+
+# Force return from the server in JSON format
+HEADERS = {'accept': 'application/json'}
+
+# This URL locates the ENCODE biosample with accession number ENCBS000AAA
+URL = "https://www.encodeproject.org/search/?" \
+      "type=experiment&" \
+      "format=json&" \
+      "field=biosample_summary&" \
+      "field=lab.title&" \
+      "field=biosample_term_name&" \
+      "field=assay_term_name&" \
+      "field=target.title&" \
+      "field=files.file_format&" \
+      "field=files.output_type&" \
+      "field=files.href&" \
+      "field=files.replicate.technical_replicate_number&" \
+      "field=files.replicate.biological_replicate_number&" \
+      "field=files.assembly&" \
+      "field=files.accession&" \
+      "limit=all"
+
+# GET the object
+response = requests.get(URL, headers=HEADERS)
+
+# Extract the JSON response as a python dict
+response_json_dict = response.json()
+
+# Graph object
+graph = response_json_dict['@graph']
+
+# Dicitionary for results
+
+results = {}
+
+for record in graph:
+
+    id = record["@id"]
+    experiment = id[13:][:-1]
+    if 'biosample_summary' in record:
+        biosample = record['biosample_summary']
+    else:
+        biosample = ''
+    assay_type = record['assay_term_name']
+    lab = record['lab']['title']
+    if 'target' in record:
+        target = record['target']['title'].replace('(Homo sapiens)', '')
+    else:
+        target = ''
+
+    # print(cell_type + '\t' + assay_type + '\t' + target)
+    if 'files' in record:
+        for file in record['files']:
+            if 'assembly' in file and 'href' in file:
+
+                assembly = file['assembly']
+                format = file['file_format']
+                output_type = file['output_type']
+                accession = file['accession']
+
+                if format == 'tsv':
+                    if assay_type.lower() != 'hic':
+                        continue;
+
+
+                bio_rep = ''
+                tech_rep = ''
+                if 'replicate' in file:
+                    rep = file['replicate']
+                    if 'bioligcal_replicate_number' in rep:
+                        bio_rep = rep['bioligcal_replicate_number']
+                    if 'technical_replicate_number' in rep:
+                        tech_rep = rep['technical_replicate_number']
+
+                if assembly in results:
+                    r = results[assembly]
+                else:
+                    r = []
+                    results[assembly] = r
+
+                if format in FORMATS:
+                    r.append(id + '\t' + assembly + '\t' + biosample + '\t' + assay_type + '\t' + target + '\t' +
+                             str(bio_rep) + '\t' + str(tech_rep) + '\t' + output_type + '\t' + format + '\t' +
+                             lab + '\t' + file['href'] +  '\t' + accession + '\t' + experiment)
+
+
+for a in list(results):
+
+    fname = 'output/' + a + ".txt"
+
+    with open(fname, 'w') as f:
+
+        print('ID\tAssembly\tBiosample\tAssayType\tTarget\tBioRep\tTechRep\tOutputType\tFormat\tLab\tHREF\tAccession\tExperiment', file=f)
+
+        r = results[a]
+
+        for x in r:
+            print(x, file=f)
+
